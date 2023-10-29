639a0832626d983c4031db7b3377b573caa84e9a
chapeach <chaipisit_ntu@hotmail.com>
2023-10-29 17:57:13 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 
 from login import *
 from home import *
-from check_act import *
 
 app = Flask(__name__)
 
@@ -12,7 +11,6 @@ app.permanent_session_lifetime = timedelta(minutes=30)
 
 app.register_blueprint(login)
 app.register_blueprint(home)
-app.register_blueprint(check_act)
 
 #if __name__ == "__main__":
 #    app.run(debug=True, host="0.0.0.0")
