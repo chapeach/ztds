562a703449cb0baa412fc7b6ff9e420935184ead
chapeach <chaipisit_ntu@hotmail.com>
2023-10-29 18:04:05 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,11 @@
 from flask import Flask
-from datetime import timedelta
-
-from login import *
-from home import *
 
 app = Flask(__name__)
 
-app.secret_key = "1234"
-app.permanent_session_lifetime = timedelta(minutes=30)
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-app.register_blueprint(login)
-app.register_blueprint(home)
-
-#if __name__ == "__main__":
-#    app.run(debug=True, host="0.0.0.0")
+@app.route('/about')
+def about():
+    return 'About'
