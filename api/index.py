fd3045752262b86012c0ecdfc46ac1432ac9437e
chapeach <chaipisit_ntu@hotmail.com>
2023-11-02 22:00:43 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask
 #from datetime import timedelta
 
-from login import *
+#from login import *
 #from home import *
 #from check_act import *
 
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '11'
+    return '1'
 
 #app.register_blueprint(login)
 #app.register_blueprint(home)
