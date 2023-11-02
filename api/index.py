c4c0ecb11e5e06abb8c92f789a878244c829d4c1
chapeach <chaipisit_ntu@hotmail.com>
2023-11-02 21:49:30 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from datetime import timedelta
+#from datetime import timedelta
 
 #from login import *
 #from home import *
@@ -7,10 +7,10 @@ from datetime import timedelta
 
 app = Flask(__name__)
 
-app.secret_key = "1234"
-app.permanent_session_lifetime = timedelta(minutes=30)
+#app.secret_key = "1234"
+#app.permanent_session_lifetime = timedelta(minutes=30)
 
-@app.route('/5678')
+@app.route('/')
 def home():
     return '111'
 
