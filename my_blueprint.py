ed08326653af09abdf3f8e503fe4a50bf4af7eb4
chapeach <chaipisit_ntu@hotmail.com>
2023-11-04 22:07:53 +0700

    Update my_blueprint.py

--- my_blueprint.py
+++ my_blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 
 my_blueprint = Blueprint("my_blueprint", __name__)
 
-@my_blueprint.route("/api", methods=["GET"])
+@my_blueprint.route("/", methods=["GET"])
 def api_route():
     data = {"message": "Hello from Blueprint API!"}
     return jsonify(data)
